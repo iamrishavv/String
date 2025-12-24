64b3812b42ad33d4ae2dcd275a4de42bd347dfed
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-24 22:30:29 +0530

    Add files via upload

---
+++ stringBuffer.java
@@ -0,0 +1,10 @@
+package String;
+
+public class stringBuffer {
+    public static void main(String[] args) {
+        StringBuffer s =new StringBuffer("rishav");
+        s.append("kumar");
+        s.reverse();
+        System.out.println(s);
+    }
+}
